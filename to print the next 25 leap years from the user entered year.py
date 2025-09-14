3c05518e510b327c9d23876af4abe462f580098f
Bhargavi-1312 <mallepulabhargavi251@gmail.com>
2025-09-14 22:07:55 +0530

    Delete to print the next 25 leap years from the user entered year.py

--- to print the next 25 leap years from the user entered year.py
+++
@@ -1,7 +0,0 @@
-n=int(input("Enter a year: "))
-leap_years = []
-while len(leap_years) < 25:
-    if (n % 4 == 0 and n % 100 != 0) or (n % 400 == 0):
-        leap_years.append(n)
-    n += 1
-print("The next 25 leap years are:", leap_years)
