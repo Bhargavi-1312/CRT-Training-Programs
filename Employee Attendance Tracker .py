c387451232ebcaa1a8967962c601875c865e6ba3
Bhargavi-1312 <mallepulabhargavi251@gmail.com>
2025-09-14 22:04:23 +0530

    Delete Employee Attendance Tracker .py

--- Employee Attendance Tracker .py
+++
@@ -1,7 +0,0 @@
-attendance = input("Enter attendance record (P for present, A for absent, L for late): ").upper()
-days_absent = attendance.count('A')
-late_days = attendance.count('L')
-if days_absent >=5 or late_days >= 3:
-    print("Employee is not eligible ")
-else:
-    print("Employee is eligible")    
